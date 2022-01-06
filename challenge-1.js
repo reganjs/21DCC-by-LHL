4e3653229efbfa603907c78e6c77047497a9b3f7
Regan <52940978+reganjs@users.noreply.github.com>
2022-01-06 15:12:05 -0700

    Add files via upload

---
+++ challenge-1.js
@@ -0,0 +1,6 @@
+//Your first task is to write a function that will take two string parameters and return a string containing the sender name 
+//and the message in the form specified in the instructions.
+
+const parseMessage = (origin, message) => {
+    return origin + ": " + message;
+};
