00bde2f3a7d2f372706bbd598d38201836f012a1
Regan <52940978+reganjs@users.noreply.github.com>
2022-01-10 07:41:03 -0700

    Add files via upload

---
+++ challenge-6.js
@@ -0,0 +1,7 @@
+//Create a function that takes in a roster array and an astronaut object. The function will add the astronaut to the roster and return
+//the updated roster.
+
+const addAstronautToRoster = (roster, astronaut) => {
+    roster.push(astronaut);
+    return roster;
+};
