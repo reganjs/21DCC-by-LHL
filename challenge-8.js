9f2a13ac7c807b9b84039fac8d5c520427a738bf
Regan <52940978+reganjs@users.noreply.github.com>
2022-01-11 11:30:13 -0700

    Add files via upload

---
+++ challenge-8.js
@@ -0,0 +1,8 @@
+//Your task is to create a function that takes in a roster array, and will calculate the amount of astronauts in the given roster list 
+//and return said amount.
+
+//Sometimes things are just simple, code doesn't need to be complicated!
+
+const countActiveAstronauts = (roster) => {
+    return roster.length
+};
